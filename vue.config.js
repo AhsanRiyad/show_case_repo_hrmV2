6997940da482c99b0083893ef0edfa7ef2e80ccc
unknown <riyad298@gmail.com>
2020-05-18 15:56:49 +0600

    pwa added

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,31 @@ module.exports = {
 
   },
   devServer: { port: 3000 },
-  publicPath: process.env.NODE_ENV === 'production' ? '/hrm-babl-v2/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/hrm-babl-v2/' : '/',
+
+
+
+  pwa: {
+    name: 'NDC92',
+    start_url: './profile',
+    themeColor: '#6200ea',
+    backgroundColor: '#6200ea',
+    msTileColor: '#6200ea',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#6200ea',
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
+    },
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
 
 }
